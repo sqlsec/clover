60d0a39cb71dc0174049ff9cb26b09c3b40312ff
国光 <1960015980@qq.com>
2019-03-20 16:26:43 +0800

    联想小新Air13 联想IdeaPad 710S i5-6200U 10.13

---
+++ Lenovo_联想/联想小新Air13 联想IdeaPad 710S i5-6200U 10.13/CLOVER/kexts/Other/Lilu.kext/Contents/Resources/Headers/kern_config.hpp
@@ -0,0 +1,19 @@
+//
+//  kern_config.hpp
+//  Lilu
+//
+//  Copyright © 2016-2017 vit9696. All rights reserved.
+//
+
+#ifndef kern_config_hpp
+#define kern_config_hpp
+
+// As for now claim to support all the features
+#define LILU_KEXTPATCH_SUPPORT 1
+#define LILU_COMPRESSION_SUPPORT 1
+
+// Use these in plugins in Xcode Project Preprocessor Macros section
+// #define LILU_CUSTOM_IOKIT_INIT 1
+// #define LILU_CUSTOM_KMOD_INIT 1
+
+#endif /* kern_config_hpp */
